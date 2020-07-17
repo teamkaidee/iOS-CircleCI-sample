eaded966a82e4e8a0585005a95a74e9c222068f2
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2020-07-17 09:07:35 +0700

    Initial Commit

---
+++ SampleCI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleCI
+//
+//  Created by Wasith Theerapattrathamrong on 17/7/2563 BE.
+//  Copyright © 2563 Wasith Theerapattrathamrong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
