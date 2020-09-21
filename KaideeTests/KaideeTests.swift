04bc2035cb3df88406b747d1473ee876ac12f954
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2020-09-21 10:52:35 +0700

    [ABC-2] style: init swiftformat

--- KaideeTests/KaideeTests.swift
+++ KaideeTests/KaideeTests.swift
@@ -1,32 +1,24 @@
-//
-//  KaideeTests.swift
-//  KaideeTests
-//
-//  Created by Wasith Theerapattrathamrong on 17/7/2563 BE.
-//  Copyright © 2563 Wasith Theerapattrathamrong. All rights reserved.
-//
-
 @testable import Kaidee
 import XCTest
 
 class KaideeTests: XCTestCase {
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  override func setUpWithError() throws {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+  override func tearDownWithError() throws {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+  func testExample() throws {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+  }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+  func testPerformanceExample() throws {
+    // This is an example of a performance test case.
+    measure {
+      // Put the code you want to measure the time of here.
     }
+  }
 }
